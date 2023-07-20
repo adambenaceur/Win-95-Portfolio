08adfb340dc51b8a8eab22d734bd4266e66cf53b
adambenaceur <benaceur.adam@gmail.com>
2023-07-19 18:24:44 -0600

    rm music

--- src/components/applications/Portfolio.tsx
+++ src/components/applications/Portfolio.tsx
@@ -7,7 +7,6 @@ import Experience from '../showcase/Experience';
 import Projects from '../showcase/Projects';
 import Contact from '../showcase/Contact';
 import SoftwareProjects from '../showcase/projects/Software';
-import MusicProjects from '../showcase/projects/Music';
 import ArtProjects from '../showcase/projects/Art';
 import VerticalNavbar from '../showcase/VerticalNavbar';
 import useInitialWindowSize from '../../hooks/useInitialWindowSize';
@@ -43,10 +42,7 @@ const ShowcaseExplorer: React.FC<ShowcaseExplorerProps> = (props) => {
                             path="/projects/software"
                             element={<SoftwareProjects />}
                         />
-                        <Route
-                            path="/projects/music"
-                            element={<MusicProjects />}
-                        />
+                        
                         <Route path="/projects/art" element={<ArtProjects />} />
                     </Routes>
                 </div>
